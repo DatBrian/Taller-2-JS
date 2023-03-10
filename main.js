43dba3f77efec7ec65e2d9a9334861643354a9a0
DatBrian <vikard.2005@hotmail.com>
2023-03-10 09:15:58 -0500

    feat: :rocket: Ejercicio 4
    En este bloque se muestra el ejercicio 4.  Para empezar se definieron 3 arrays, en el primero se almacenarán los datos de las personas, en el segundo solo los nombres y en el tercero las edades. Luego se crea una función que va añadir cada persona nueva con un nombre y una edad especificada por el usuario, esta función se ejecutará 3 veces gracias al for que está después de esta. También hay dos funciones que van a agregar los nombres y las edades por separado en sus respectivos arrays . Terminando se va a crear una variable en la que se buscará la posición de la edad máxima que nos servirá para determinar a su vez la de el nombre que corresponde a esa eda y para finalizar se muestra el resultado en pantalla, esto busca tanto el dato del nombre como el de la edad con el mismo indice que se busco anteriormente.

--- main.js
+++ main.js
@@ -1,7 +1,31 @@
-//3. Construir el algoritmo para determinar el voltaje de un
-//circuito a partir de la resistencia y la intensidad de corriente.
+//4. Construir el algoritmo que solicite el nombre y edad de 3
+//personas y determine el nombre de la persona con mayor edad.
 
-let resistencia = parseFloat(prompt("Ingrese la resistencia: "));
-let intensidad = parseFloat(prompt("Ingrese la intensidad: "));
-let voltaje = (intensidad * resistencia);
-alert(`El voltaje es: ${voltaje}`);
+let personas = []
+let nombres = []
+let edades = []
+
+function addPerson(){
+    let nombre = prompt("Ingrese nu nombre: ");
+    let edad = parseInt(prompt("Ingrese edad: "));
+    personas.push({nombre, edad})
+}
+for (let i = 0; i < 3; i++){
+    addPerson()
+}
+
+function listaNombres(){
+    for (let i of personas)
+        nombres.push(i.nombre);
+}
+
+function listaEdades(){
+    for (let i of personas)
+        edades.push(i.edad);
+}
+
+listaNombres();
+listaEdades();
+
+let nombreMayor = edades.indexOf(Math.max(...edades));
+console.log(`La persona mayor es: ${nombres[nombreMayor]} y tiene ${Math.max(...edades)} años`);
