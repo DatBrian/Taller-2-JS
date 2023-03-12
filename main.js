f890134e7c8414a0d252a9b1a4dbea07744e8e63
DatBrian <vikard.2005@hotmail.com>
2023-03-11 20:24:02 -0500

    feat: :rocket: Ejercicio 7
    En este bloque se muestra el ejercicio 7. Para empezar se declaran las variables que contendrán los datos dados por el usuario a través de los prompt, estos datos serán nombre, precio y cantidad. Al tener los datos se creará otra variable en la que se realizará la operación de multiplicar el precio de los artículos por la cantidad de artículos que el usuario desea y para terminar se muestra con un alert el precio total a pagar.

--- main.js
+++ main.js
@@ -1,79 +1,9 @@
-// 6. Construir el algoritmo en Javascript para un programa
-// para cualquier cantidad de estudiantes que lea el nombre,
-// el sexo y la nota definitiva y halle al estudiante con la mayor
-// nota y al estudiante con la menor nota y cuantos eran
-// hombres y cuantos mujeres.
+// 7. Programa que pida el ingreso del nombre y precio de un artículo y la
+// cantidad que lleva el cliente. Mostrar lo que debe pagar el comprador
+// en su factura.
 
-let estudiantes = [];
-let nombres = [];
-let notas = [];
-let hombres = 0;
-let mujeres = 0;
-
-function actualizarDatos(nombre, nota, sexo) {
-    estudiantes.push({
-        nombre: nombre,
-        sexo: sexo,
-        nota: nota
-    });
-    nombres.push(nombre);
-    notas.push(nota);
-}
-function pedirDatos(){
-    let nombre = prompt("Ingrese su nómbre: ");
-    let nota = parseInt(prompt("Ingrese su nota: "));
-    let sexo = pedirSexo();
-    actualizarDatos(nombre, nota, sexo);
-}
-
-function pedirSexo(){
-    let sexo = prompt(`Ingrese su sexo ("M" o "F"):`)
-    if (sexo === "M") {
-        hombres++
-        sexo = "Masculino"
-        return sexo;
-
-    }
-    else if (sexo === "F"){
-        mujeres++;
-        sexo = "Femenino"
-        return sexo;
-    }
-    else {
-        return sexo = "indefinido"
-    }
-}
-
-function calcularMayor (){
-    let notaMayor = Math.max(...notas);
-    let estudianteMayor = estudiantes.find(estudiante => estudiante.nota === notaMayor);
-    alert(`El estudiante con la nota mayor es: ${estudianteMayor["nombre"]}`);
-}
-
-function calcularMenor (){
-    let notaMenor = Math.min(...notas);
-    let estudianteMenor = estudiantes.find(estudiante => estudiante.nota === notaMenor);
-    alert(`El estudiante con la nota menor es: ${estudianteMenor["nombre"]}`);
-}
-
-function mostrarDatos(){
-    console.log(estudiantes);
-    console.log(nombres);
-    console.log(notas);
-    console.log(hombres);
-    console.log(mujeres);
-    calcularMayor();
-    calcularMenor();
-    alert(`Hay ${hombres} hombres y ${mujeres} mujeres`)
-}
-
-function programa (){
-    pedirDatos()
-    if (confirm("¿Quieres continuar")){
-        programa();
-    }else{
-        mostrarDatos();
-    }
-}
-
-programa();
+let nombre = prompt("Ingrese el nombre del artículo que desea comprar: ");
+let precio = parseFloat(prompt("Ingrese el precio del artículo que desea comprar: "));
+let cantidad = parseInt(prompt("Ingrese la cantidad de artículos que desea comprar: "));
+let total = parseFloat(precio * cantidad)
+alert(`El total a pagar es: $${total}`);
