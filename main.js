6279695d63735e7df900468ab31e7452b8771799
DatBrian <vikard.2005@hotmail.com>
2023-03-12 22:47:27 -0500

    feat: :rocket: Ejercicio 9
    En este bloque se muestra el ejercicio 9. Para empezar se declaran dos arrays vacíos de los cuales el primero contendrá los datos de las atletas y el segundo solo las marcas para facilitar los cálculos requeridos, también se guarda en una variable el record. Para empezar la resolución del ejercicio primero se declara una función para actualizar los datos; esta función añadirá los datos ded las atletas a su respectivo array mediante un push y también añadirá las marcas, esta función ira dentro de otra función en la que se piden los datos al usuario por medio de prompts. Luego se tiene una función para calcular las marcas mayores y las ganadoras, para esto se de clara la marca mayor utilizando el método max dentro del array de las marcas; con esto ya se puede buscar a la atleta que coincide con esta marca usando el método find y por último se utiliza un condicional if para que si la marca mayor supera al record muestre el respectivo premio mientras que de no ser así solo mostrará la atleta ganadora de la medalla de oro. Para terminar se tiene una función con todo el programa en la que se encuentran la función pedir datos y además un condicional para preguntarle al usuario si quiere seguir añadiendo atletas, en caso de que no sea asi se calculará la marca mayor y se terminará el programa. Para ejecutar todo solo se llama a la función programa que acabamos de especificar.

--- main.js
+++ main.js
@@ -1,13 +1,46 @@
-// 8. Programa que Ingrese por teclado:
-// a. el valor del lado de un cuadrado para mostrar por pantalla el
-// perímetro del mismo
-// b. la base y la altura de un rectángulo para mostrar el área del
-// mismo
+// 9. N atletas han pasado a finales en salto triple en los juegos
+// olímpicos femenino de 2022. Diseñe un programa que pida por
+// teclado los nombres de cada atleta finalista y a su vez, sus
+// marcas del salto en metros. Informar el nombre de la atleta
+// campeona que se quede con la medalla de oro y si rompió
+// récord, reportar el pago que será de 500 millones. El récord
+// esta en 15,50 metros.
 
-lado = parseFloat(prompt("Ingrese el lado de un cuadrado: "));
-perimetro = lado *  4
-alert(`El perímetro es igual al: ${perimetro}`);
-base = parseFloat(prompt("Ingrese la base del rectángulo: "));
-altura = parseFloat(prompt("Ingrese la altura del rectángulo: "));
-area = base * altura
-alert(`El área del rectángulo es igual a: ${area}`)
+let atletas = [];
+let marcas = [];
+let record = 15.50
+
+function actualizarDatos(nombre, marca){
+    atletas.push({
+        nombre: nombre,
+        marca: marca
+    });
+    marcas.push(marca);
+}
+function pedirDatos(){
+    let nombre = prompt("Ingrese su nombre: ");
+    let marca = parseFloat(prompt("Ingrese su marca: "));
+    actualizarDatos(nombre, marca);
+}
+function calcularMayor(){
+    let marcaMayor = Math.max(...marcas);
+    let atletaMayor = atletas.find(atleta => atleta.marca === marcaMayor);
+    if (marcaMayor > record){
+        alert(`La atleta ${atletaMayor["nombre"]} ha superado el record actual con una marca de ${marcaMayor} ganando el premio de 500 millones`);
+    }
+    else{
+        alert(`La ganadora de la medalla de oro con la mejor marca es: ${atletaMayor["nombre"]}`)
+    }
+}
+
+function programa(){
+    pedirDatos();
+    if(confirm("¿Agregar otra atleta?")){
+        programa();
+    }
+    else {
+        calcularMayor();
+    }
+}
+
+programa();
